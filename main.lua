928591435e0b78d67c18031a5d89a6e60f2d97af
Hayman Ati <hayman-35@hotmail.com>
2019-03-08 20:36:46 -0500

    Changed the body of the code to make more simple

--- main.lua
+++ main.lua
@@ -1,10 +1,13 @@
 local tapCount = 0
-local tapToStartLabel
+local getReady
+local gameOver
+local board
 local physics = require( "physics" )
+local widget = require( "widget" )
+
 local background = display.newImageRect( "background.png", 360, 570 )
 background.x = display.contentCenterX
 background.y = display.contentCenterY
-
 
 local platform = display.newImageRect( "platform.png", 500, 20 )
 platform.x = display.contentCenterX
@@ -20,13 +23,11 @@ local platform2 = display.newImageRect( "platform1.png", 20, 2000 )
 platform2.x = display.contentCenterX+170
 platform2.y = display.contentHeight
 
-local ball = display.newImageRect("ball.png",100,100)
-ball.x = display.contentCenterX
-ball.y = display.contentCenterY
-ball.id = "ball"
-ball.isball = true
-
 local tapText = display.newText( tapCount, display.contentCenterX, 20, native.systemFont, 40 )
+
+local GameoverLabel = display.newText("Game Over",display.contentCenterX, 200, native.systemFont, 40)
+GameoverLabel:setFillColor(0,0,1)
+GameoverLabel.isVisible = false
 
 -- The menu
 local function Menu()
@@ -36,14 +37,17 @@ end
 
 Menu()
 
+local ball = display.newImageRect("ball.png",100,100)
+ball.x = display.contentCenterX
+ball.y = display.contentCenterY
 
 -- Starting the game
-local function startGame( event )
-  display.remove(tapToStartLabel)
-  if ( event.phase == "began" ) then
-    tapCount = 1000
-    display.remove(tapToStartLabel)
-  end
+local function startGame()
+  ball.x = display.contentCenterX
+  ball.y = display.contentCenterY
+  ball.id = "ball"
+  ball.isball = true
+display.remove(tapToStartLabel)
 physics.start()
 physics.setGravity( 0, 20 )
 physics.addBody( platform, "static" )
@@ -51,43 +55,49 @@ physics.addBody( platform1, "static" )
 physics.addBody( platform2, "static" )
 physics.addBody( ball, "dynamic", { radius=50, bounce=-1 } )
 end
+--background:addEventListener("tap", startGame )
+--ball:addEventListener( "tap", startGame )
 
+local function initGame()
+  transition.to( ball, { time=300, x=xBird, y=yBird, rotation = 0 } )
+  transition.to( getReady, { time=600, y=yReady, transition=easing.outBounce, onComplete=prompt   } )
+  ball.rotation =  -30*math.atan(30)
 
-local function pushBall()
+end
+
+function ball.tap()
 ball:applyForce(math.random()*50-25, -80, ball.x,ball.y)
 ball:applyTorque( math.random()*50 - 15 )
 tapCount = tapCount + 1
 tapText.text = tapCount
 end
+ball:addEventListener( "tap" )
 
 
-function GameOver(event)
-    if event.phase == "began" then
-        local target = event.other
-            if target.isplatform then
+function platform.collision(event,self)
+  if( event.phase == "began" and event.other.isplatform ) then
                 tapCount = 0
                 tapText.text = tapCount
-                tapToStartLabel.text = "Game Over"
-                tapToStartLabel.isVisible = true
-                tapToStartLabel:setFillColor( 1, 0, 0 )
-                tapToStartLabel.text = "Game Over"
-                tapToStartLabel.isVisible = true
-                tapToStartLabel:setFillColor( 1, 0, 0 )
-
-            end
-    end
+                GameoverLabel.isVisible = true
+                --physics.pause()
+              end
 end
+platform:addEventListener( "collision" )
 
-background:addEventListener("tap", startGame )
-ball:addEventListener( "tap", startGame )
-ball:addEventListener( "tap", pushBall )
-ball:addEventListener( "collision", GameOver )
-
-
-local function call_VK_event()
-    local args={}
-    args.user_id='33251324'
-    args.activity_id=2
-    args.value=tapCount
-    vk.api('secure.addAppEvent', args)
+local function Restart(event)
+  if ( event.phase == "began" ) then
+            GameoverLabel.isVisible = false
+    elseif ( event.phase == "ended" ) then
+    -- physics.pause()
+     restartlabel = display.newText("Tap To Start",display.contentCenterX, 200, native.systemFont, 40)
+     restartlabel:setFillColor(0,0,1)
+     --ball.x = display.contentCenterX
+    -- ball.y = display.contentCenterY
 end
+--restartlabel.isVisible = false
+          end
+          background:addEventListener( "touch", Restart )
+
+          -- Start application point
+          startGame()
+          initGame()
