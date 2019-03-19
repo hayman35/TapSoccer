824d8f63bc38f20d44753a46892c7c738a1af364
Hayman Ati <hayman-35@hotmail.com>
2019-03-19 19:32:11 -0400

    Got the collisions to work

--- main.lua
+++ main.lua
@@ -12,8 +12,7 @@ background.y = display.contentCenterY
 local platform = display.newImageRect( "platform.png", 500, 20 )
 platform.x = display.contentCenterX
 platform.y = display.contentHeight+44
-platform.id = "platform"
-platform.isplatform = true
+platform.myName = "platform"
 
 local platform1 = display.newImageRect( "platform1.png", 20, 2000 )
 platform1.x = display.contentCenterX-170
@@ -23,31 +22,41 @@ local platform2 = display.newImageRect( "platform1.png", 20, 2000 )
 platform2.x = display.contentCenterX+170
 platform2.y = display.contentHeight
 
-local tapText = display.newText( tapCount, display.contentCenterX, 20, native.systemFont, 40 )
+
+local ball = display.newImageRect("ball.png",100,100)
+ball.x = display.contentCenterX
+ball.y = display.contentCenterY
+ball.myName = "ball"
+
+
+local score = display.newText( tapCount, display.contentCenterX, 20, native.systemFont, 40 )
 
 local GameoverLabel = display.newText("Game Over",display.contentCenterX, 200, native.systemFont, 40)
 GameoverLabel:setFillColor(0,0,1)
 GameoverLabel.isVisible = false
 
--- The menu
-local function Menu()
-tapToStartLabel = display.newText("Tap To Start",display.contentCenterX, 200, native.systemFont, 40)
+tapToStartLabel = display.newText("Tap Here To Start",display.contentCenterX, 450, native.systemFont, 40)
 tapToStartLabel:setFillColor(0,0,1)
+
+
+local function balloonUp(event)
+	if event.phase == "began" then
+		print("tapStarted")
+
+	elseif event.phase == "ended" then
+		print("tapEnded")
+end
 end
 
-Menu()
-
-local ball = display.newImageRect("ball.png",100,100)
-ball.x = display.contentCenterX
-ball.y = display.contentCenterY
-
 -- Starting the game
-local function startGame()
+local function startGame(event)
   ball.x = display.contentCenterX
   ball.y = display.contentCenterY
-  ball.id = "ball"
-  ball.isball = true
-display.remove(tapToStartLabel)
+  if event.phase == "began" then
+  		-- Start the game here
+  	elseif event.phase == "ended" then
+      display.remove(tapToStartLabel)
+    end
 physics.start()
 physics.setGravity( 0, 20 )
 physics.addBody( platform, "static" )
@@ -55,49 +64,27 @@ physics.addBody( platform1, "static" )
 physics.addBody( platform2, "static" )
 physics.addBody( ball, "dynamic", { radius=50, bounce=-1 } )
 end
---background:addEventListener("tap", startGame )
---ball:addEventListener( "tap", startGame )
+tapToStartLabel:addEventListener( "touch", startGame )
 
-local function initGame()
-  transition.to( ball, { time=300, x=xBird, y=yBird, rotation = 0 } )
-  transition.to( getReady, { time=600, y=yReady, transition=easing.outBounce, onComplete=prompt   } )
-  ball.rotation =  -30*math.atan(30)
-
-end
 
 function ball.tap()
 ball:applyForce(math.random()*50-25, -80, ball.x,ball.y)
 ball:applyTorque( math.random()*50 - 15 )
 tapCount = tapCount + 1
-tapText.text = tapCount
+score.text = tapCount
 end
 ball:addEventListener( "tap" )
 
-
-function platform.collision(event,self)
-  if( event.phase == "began" and event.other.isplatform ) then
-                tapCount = 0
-                tapText.text = tapCount
-                GameoverLabel.isVisible = true
-                --physics.pause()
-              end
+local function onLocalCollision( self, event )
+    if ( event.phase == "began" ) then
+    score.text = 0
+    elseif ( event.phase == "ended" ) then
+        physics.pause()
+        GameoverLabel.isVisible = true
+      --startGame()
+    end
 end
+platform.collision = onLocalCollision
 platform:addEventListener( "collision" )
 
-local function Restart(event)
-  if ( event.phase == "began" ) then
-            GameoverLabel.isVisible = false
-    elseif ( event.phase == "ended" ) then
-    -- physics.pause()
-     restartlabel = display.newText("Tap To Start",display.contentCenterX, 200, native.systemFont, 40)
-     restartlabel:setFillColor(0,0,1)
-     --ball.x = display.contentCenterX
-    -- ball.y = display.contentCenterY
-end
---restartlabel.isVisible = false
-          end
-          background:addEventListener( "touch", Restart )
-
           -- Start application point
-          startGame()
-          initGame()
